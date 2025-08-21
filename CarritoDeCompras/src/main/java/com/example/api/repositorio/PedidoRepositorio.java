2f7e8a10167a1cbbe7ef40b8575f96a6242a0a10
RosalesTello <ln75307953@idat.pe>
2025-08-21 02:23:47 -0500

    Delete CarritoDeCompras directory

--- CarritoDeCompras/src/main/java/com/example/api/repositorio/PedidoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repositorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.api.entidades.Pedido;
-
-@Repository
-public interface PedidoRepositorio extends JpaRepository<Pedido, Integer> {
-
-}
